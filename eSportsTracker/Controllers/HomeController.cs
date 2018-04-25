a8dc4379fa2e829acc656617ad744d09c94156f9
Cambron Johnson <johnsocp@rose-hulman.edu>
2018-04-24 22:45:49 -0400

    Began Project

---
+++ eSportsTracker/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eSportsTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "CSSE 333 Database Project: eSportsTracker";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Email:";
+
+            return View();
+        }
+    }
+}
