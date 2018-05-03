982c40363e0f5bcea4e0979116b19db114fdb764
Aaron Michael <michaeap@rose-hulman.edi>
2018-05-03 18:46:27 -0400

    did stuff

---
+++ eSportsTracker/Controllers/MatchesController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eSportsTracker.Models;
+
+namespace eSportsTracker.Views.VideoGames
+{
+    public class MatchesController : Controller
+    {
+        private EsportsTrackerEntities1 db = new EsportsTrackerEntities1();
+
+        // GET: Matches
+        public ActionResult Index()
+        {
+            var matches = db.Matches.Include(m => m.SoloMatch).Include(m => m.TeamMatch);
+            return View(matches.ToList());
+        }
+
+        // GET: Matches/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Match match = db.Matches.Find(id);
+            if (match == null)
+            {
+                return HttpNotFound();
+            }
+            return View(match);
+        }
+
+        // GET: Matches/Create
+        public ActionResult Create()
+        {
+            if (Session["LoggedIn"] == null)
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+            ViewBag.MatchID = new SelectList(db.SoloMatches, "MatchID", "MatchID");
+            ViewBag.MatchID = new SelectList(db.TeamMatches, "MatchID", "MatchID");
+            return View();
+        }
+
+        // POST: Matches/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MatchID,TimePlayed")] Match match)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Matches.Add(match);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MatchID = new SelectList(db.SoloMatches, "MatchID", "MatchID", match.MatchID);
+            ViewBag.MatchID = new SelectList(db.TeamMatches, "MatchID", "MatchID", match.MatchID);
+            return View(match);
+        }
+
+        // GET: Matches/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (Session["LoggedIn"] == null)
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Match match = db.Matches.Find(id);
+            if (match == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MatchID = new SelectList(db.SoloMatches, "MatchID", "MatchID", match.MatchID);
+            ViewBag.MatchID = new SelectList(db.TeamMatches, "MatchID", "MatchID", match.MatchID);
+            return View(match);
+        }
+
+        // POST: Matches/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MatchID,TimePlayed")] Match match)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(match).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MatchID = new SelectList(db.SoloMatches, "MatchID", "MatchID", match.MatchID);
+            ViewBag.MatchID = new SelectList(db.TeamMatches, "MatchID", "MatchID", match.MatchID);
+            return View(match);
+        }
+
+        // GET: Matches/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (Session["LoggedIn"] == null)
+            {
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Match match = db.Matches.Find(id);
+            if (match == null)
+            {
+                return HttpNotFound();
+            }
+            return View(match);
+        }
+
+        // POST: Matches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Match match = db.Matches.Find(id);
+            db.Matches.Remove(match);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
